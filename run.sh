14d2a9764de40a0298425e90952f7212cb530e8a
huzhe2020 <huzhe2020@gmail.com>
2026-01-17 11:21:00 +0800

    add lmstudio

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-export T_LLM_URL="http://localhost:11434/v1"
-#export T_MODEL_NAME="deepseek-r1:14b"
-export T_MODEL_NAME="deepseek-r1:14b"
-export T_API_KEY="ollama"
+# ollama 
+# export T_LLM_URL="http://localhost:11434/v1"
+# export T_MODEL_NAME="deepseek-r1:70b"
+# export T_API_KEY="ollama"
+
+#lm studio
+export T_LLM_URL="http://localhost:1234/v1"
+export T_MODEL_NAME="deepseek/deepseek-r1-distill-qwen-32b"
+export T_API_KEY="lmstudio"
 python run_benchmarks.py \
     --llm_url ${T_LLM_URL} \
     --api_key ${T_API_KEY} \
